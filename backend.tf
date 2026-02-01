7627bcb9ab594ec274fb8c5f73743f389eb992f2
Nouf283 <noufawal0311@gmail.com>
2026-02-01 16:27:27 +1100

    updated

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "capstone-terraform-state-bucket"
+    bucket         = "noufa-capstone-tf-state-ap-southeast-1"
     key            = "kubeadm/terraform.tfstate"
     region         = "ap-southeast-1"
     dynamodb_table = "terraform-locks"
